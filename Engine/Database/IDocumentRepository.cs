4eb06d4c04db3bce1f906a2973b2ae498265d150
Jorge Figueroa <yknx4@live.com.mx>
2015-03-29 19:28:07 +0900

    Engine Database Models

---
+++ Engine/Database/IDocumentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Model;
+
+namespace Engine.Database
+{
+    public interface IDocumentRepository
+    {
+        LogicalView Get(int id);
+        LogicalView Get(String url);
+        void Insert(LogicalView input);
+        void InsertBatch(IEnumerable<LogicalView> input);
+    }
+}
