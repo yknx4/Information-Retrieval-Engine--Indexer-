e15f9a68e52ab5e68508ca45058a993d04792742
Jorge Figueroa <yknx4@live.com.mx>
2015-04-01 15:21:00 +0900

    Engine Constants Update

--- Engine/Database/Constants.cs
+++ Engine/Database/Constants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
+﻿using MySql.Data.MySqlClient;
 
 namespace Engine.Database
 {
@@ -32,18 +26,41 @@ namespace Engine.Database
             }
         }
 
+        public static int TermIdMaximumTries = 3;
+
+        public static double CommitInterval = 2000;
+        public static double RetryInterval = 10000;
+        public static int QueriesPerTransaction = 100;
+        public static int MaxTimeout = 2147483;
 
 
         public abstract class Queries
         {
             public static string InsertDocumentQuery =
-                "INSERT INTO `" + DataContract.DocumentEntry.TableName + "` (`" + DataContract.DocumentEntry.Url + "`,`" + DataContract.DocumentEntry.UrlHash + "`,`" + DataContract.DocumentEntry.Title + "`) VALUES (" + Constants.Parameters.Url + ",md5(" + Constants.Parameters.Url + ")," + Constants.Parameters.Title + ");";
+                "INSERT IGNORE INTO `" + DataContract.DocumentEntry.TableName + "` (`" + DataContract.DocumentEntry.Url + "`,`" + DataContract.DocumentEntry.UrlHash + "`,`" + DataContract.DocumentEntry.Title + "`) VALUES (" + Parameters.Url + ",md5(" + Parameters.Url + ")," + Parameters.Title + ");";
+            public static string InsertTermQuery =
+                "INSERT IGNORE INTO `" + DataContract.TermEntry.TableName + "` (`" + DataContract.TermEntry.Value + "`) VALUES (" + Parameters.Value + ");";
+
+            public static string SelectAllTermsQuery = "SELECT * FROM " + DataContract.TermEntry.TableName + ";";
+
+            public static string SelectDocumentIdFromUrlQuery =
+                "SELECT COALESCE((SELECT `" + DataContract.DocumentEntry.Id + "` FROM `" + DataContract.DocumentEntry.TableName + "` WHERE `" + DataContract.DocumentEntry.UrlHash + "` = MD5(" + Parameters.Url + ")),-1) as " + DataContract.DocumentEntry.Id + ";";
+            public static string SelectTermIdFromUrlQuery =
+               "SELECT COALESCE((SELECT `" + DataContract.TermEntry.Id + "` FROM `" + DataContract.TermEntry.TableName + "` WHERE `" + DataContract.TermEntry.Value + "` = " + Parameters.Value + "),-1) as " + DataContract.TermEntry.Id + ";";
+
+            public static string InsertWeightQuery =
+                "INSERT IGNORE INTO `" + DataContract.WeightEntry.TableName + "`(`" + DataContract.WeightEntry.TermId + "`,`" + DataContract.WeightEntry.DocumentId + "`,`" + DataContract.WeightEntry.Count + "`) values (" + Parameters.TermId + "," + Parameters.DocumentId + "," + Parameters.Count + ");";
         }
 
         public abstract class Parameters
         {
             public static string Url = "@url";
             public static string Title = "@title";
+            public static string Value = "@value";
+            public static string TermId = "@termid";
+            public static string DocumentId = "@documentid";
+            public static string Count = "@count";
+
         }
         
     }
--- Engine/Database/DataContract.cs
+++ Engine/Database/DataContract.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Engine.Database
+﻿namespace Engine.Database
 {
     abstract class DataContract
     {
@@ -16,6 +10,22 @@ namespace Engine.Database
             public static string Title = "title";
             public static string Url = "url";
             public static string UrlHash = "url_hash";
+        }
+        static public class TermEntry
+        {
+            public static string TableName = "indexes";
+            public static string Id = "id";
+            public static string Value = "name";
+        }
+
+        static public class WeightEntry
+        {
+            public static string TableName = "weights";
+            public static string Id = "id";
+            public static string TermId = "term_id";
+            public static string DocumentId = "document_id";
+            public static string Count = "count";
+
         }
     }
 }
